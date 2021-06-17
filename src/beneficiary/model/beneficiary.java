1c19abdc6803eb000559de993df111963d8e5564
donchi_herve <donchiherve1@gmail.com>
2021-06-17 10:09:34 +0100

    In the src directory, 3 classes have been added, beneficiaryservlet.java in the controller, beneficiarydao.java int the dao and beneficiary.java in the model package, this files are to link the jsp files to the database and send the information entered by the beneficiary in the correct entries of the database

---
+++ src/beneficiary/model/beneficiary.java
@@ -0,0 +1,55 @@
+package net.javaguides.beneficiary.model;
+
+public class beneficiary {
+		private String firstname;
+		private String lastname;
+		private String familysize;
+		private String address;
+		private String contact;
+		private String town;
+		private String profession;
+
+		
+		public String getFirstname() {
+			return firstname;
+		}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+		public String getLastname() {
+			return lastname;
+		}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+		public String getFamilysize() {
+			return familysize;
+		}
+		public void setFamilysize(String familysize) {
+			this.familysize = familysize;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getContact() {
+			return contact;
+		}
+		public void setContact(String contact) {
+			this.contact = contact;
+		}
+		public String getTown() {
+			return town;
+		}
+		public void setTown(String town) {
+			this.town = town;
+		}
+		public String getProfession() {
+			return profession;
+		}
+		public void setProfession(String profession) {
+			this.profession = profession;
+		}
+		}
