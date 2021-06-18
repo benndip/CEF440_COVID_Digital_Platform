99111d54dc52d9f88516a1525d9a4e0ba622ab42
Akumah Ndeh-Ngwa <ndehngwa@gmail.com>
2021-06-18 16:44:31 +0100

    goes into the same package as the servlet

---
+++ src/landingPage/Family.java
@@ -0,0 +1,18 @@
+package Geolocation;
+
+public class Family {
+
+	public String name;
+	public String infected;
+	public String total;
+	
+	
+	
+	public Family(String name, String infected, String total) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+		this.infected=infected;
+		this.total=total;
+	}
+
+}
