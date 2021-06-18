19a9278ddbaee69e916314516fc8676879527006
Kelson9 <kelsonngoe@gmail.com>
2021-06-18 09:22:11 -1100

    added the @DeclareRoles(user) annotation to specify security roloes

--- src/landingPage/LandingPageServlet.java
+++ src/landingPage/LandingPageServlet.java
@@ -6,10 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.annotation.security.DeclareRoles;
 
 /**
  * Servlet implementation class LandingPageServlet
  */
+
+@DeclareRoles("user")
 @WebServlet("/LandingPageServlet")
 public class LandingPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
